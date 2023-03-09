466c8d7f1fb3bda5e1c109f3a189286d115a8fae
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-09 11:03:16 +0900

    ?

---
+++ Test/src/mypack/Test.java
@@ -0,0 +1,33 @@
+package mypack;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("자바야 안녕, 내 이름은 솔아몬이야."); 
+		System.out.println("HELLO");
+		System.out.println("BYE");
+		System.out.println("Im hungry.");
+		System.out.printf("%d\n", 10);
+		System.out.printf("%o\n", 10);
+		System.out.printf("%x\n", 10);
+		System.out.printf("%s\n", "print string");
+		System.out.printf("%f\n", 3.2582);
+		System.out.printf("%4.2f\n", 3.2582);
+		System.out.printf("%d and %4.2f\n", 10, 3.2582);
+		
+		System.out.println("안녕하세요");
+		System.out.println("안녕" + "하세요");
+		System.out.println(2 + 4);
+		System.out.println(4.6);
+		System.out.println("문자" + 1);
+		System.out.println("문자" + 1 + 2);
+		//(밑에꺼)일단 1+2연산이 수행된 3이 나오고 그다음에 문자라고 뜬다.
+		System.out.println(1 + 2 + "문자");  
+//		System.out.println();
+		
+	}
+}
+
+class A{}
+class B{}
+class c{}
+class JBNU{}
