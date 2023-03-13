1e27d1d6b7b2d8ee9188c147625a053dc229b213
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-13 15:10:01 +0900

    ㅇ

--- java0310/src/java0310/Test.java
+++ java0310/src/java0310/Test.java
@@ -22,9 +22,15 @@ public class Test {
 		//#2.다른 자료형간의 연산
 		//int value6 = 5+3.5;
 		double value6 = 5+3.5;
-		double value7 = 5/2;
+		double value7 = 5/2; // int/int이기 때문에 2.5가 아닌 2가 나
 		System.out.println(value6);
 		System.out.println(value7);
+		
+		//@참조자료형 등가비교(번지비교)
+		String str1= new String("Hello");
+		String str2 = new String("Hello");
+		//둘이 값은 똑같아도, 번지수가 다름
+		System.out.println(str1==str2);
 		
 	}
 
