94e9c928bd9d966001019e5741ed9015d0f42f83
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-03-17 17:48:54 +0900

    ;;

--- java0310/src/java0310/RandomWalk.java
+++ java0310/src/java0310/RandomWalk.java
@@ -1,7 +1,9 @@
 package java0310;
+import java.util.Scanner;
 
 public class RandomWalk {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
         int x = 5, y = 5;
         boolean tile[][] = new boolean[11][11];
         int steps;
@@ -18,6 +20,10 @@ public class RandomWalk {
 				y++;
             tile[y][x] = true;
             
+            //쉬어가는 기능
+            System.out.print("Enter. ");
+            input.nextLine();
+	            
             System.out.println("------------ " + (steps + 1) + " --------------");
             for (int i = 0; i < 11; i++) {
             	for (int j = 0; j < 11; j++) {
